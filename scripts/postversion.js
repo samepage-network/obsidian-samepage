82b099dcd2d3957569de4d28923c1bece691fe09
David Vargas <dvargas92495@gmail.com>
2022-11-17 13:37:47 -0500

    2.0.0

--- scripts/postversion.js
+++ scripts/postversion.js
@@ -1,14 +1,10 @@
 const fs = require("fs");
 
-module.exports = () => {
-  const { version } = JSON.parse(
-    fs.readFileSync("package.json").toString()
-  );
-  fs.writeFileSync(
-    "manifest.json",
-    fs
-      .readFileSync("manifest.json")
-      .toString()
-      .replace(/"version": "[\d.-]+",/, `"version": "${version}",`)
-  );
-};
+const { version } = JSON.parse(fs.readFileSync("package.json").toString());
+fs.writeFileSync(
+  "manifest.json",
+  fs
+    .readFileSync("manifest.json")
+    .toString()
+    .replace(/"version": "[\d.-]+",/, `"version": "${version}",`)
+);
